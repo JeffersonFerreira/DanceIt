a23a33231a305d0dacfea13f781a7e69cbbeba36
Jefferson <jeffersonferreira.01@pm.me>
2023-06-27 16:23:21 -0300

    🚧 (WIP) Added snapped smooth scrolling

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,52 +8,50 @@ public class GameUI : MonoBehaviour
     [SerializeField] private List<RenderTexture> _renderTextures;
     [SerializeField] private VisualTreeAsset _listItemTemplate;
 
-    private ListView _listView;
-
-    private int _i;
     private UIDocument _doc;
+    private ListView _listView;
+    private Scroller _scroller;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-            _listView.ScrollToItem(++_i);
+    private float _lastScrollDelta = 0;
+    private VisualElement _pointerDownElement;
+    private MyViewController _viewController;
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            _listView.ScrollToItem(--_i);
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            var container = _listView.Q<VisualElement>("unity-content-container");
-            var scroller = _listView.Q<Scroller>(classes: Scroller.ussClassName);
-
-            // VisualElement visualElement = _listView.contentContainer[0];
-            // Debug.Log(visualElement);
-
-            foreach (var element in container.Children())
-            {
-                var pos = element.layout.position;
-                var hei = _listView.fixedItemHeight;
-
-                float screenVisibilityFactor;
-                if (scroller.value >= pos.y)
-                {
-                    var diff = scroller.value - pos.y;
-                    screenVisibilityFactor = (hei - diff) / hei;
-                }
-                else
-                {
-                    var diff = (scroller.value + hei) - pos.y;
-                    screenVisibilityFactor = diff / hei;
-                }
-
-                // if (f > 0.5f)
-                // {
-                //     _listView.ScrollTo(element);
-                //     return;
-                // }
-            }
-        }
-    }
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.T))
+    //     {
+    //         var container = _listView.Q<VisualElement>("unity-content-container");
+    //         var scroller = _listView.Q<Scroller>(classes: Scroller.ussClassName);
+    //
+    //         // VisualElement visualElement = _listView.contentContainer[0];
+    //         // Debug.Log(visualElement);
+    //
+    //         foreach (var element in container.Children())
+    //         {
+    //             var pos = element.layout.position;
+    //             var hei = _listView.fixedItemHeight;
+    //
+    //             float screenVisibilityFactor;
+    //             if (scroller.value >= pos.y)
+    //             {
+    //                 var diff = scroller.value - pos.y;
+    //                 screenVisibilityFactor = (hei - diff) / hei;
+    //             }
+    //             else
+    //             {
+    //                 var diff = (scroller.value + hei) - pos.y;
+    //                 screenVisibilityFactor = diff / hei;
+    //             }
+    //
+    //             // if (f > 0.5f)
+    //             // {
+    //             //     _listView.ScrollTo(element);
+    //             //     return;
+    //             // }
+    //
+    //         }
+    //     }
+    // }
 
     private void OnEnable()
     {
@@ -68,37 +66,48 @@ public class GameUI : MonoBehaviour
             });
 
         _listView = _doc.rootVisualElement.Q<ListView>("main-list");
+        _scroller = _listView.Q<Scroller>(classes: Scroller.ussClassName);
 
-        _listView.SetViewController(new MyViewController(_renderTextures, _listItemTemplate));
+        _viewController = new MyViewController(_renderTextures, _listItemTemplate);
+        _listView.SetViewController(_viewController);
+        _listView.RegisterCallback<PointerMoveEvent>(OnPointerMoveCallback);
+        _listView.RegisterCallback<PointerUpEvent>(OnPointerUpCallback);
 
         // Source must be set after controller
         _listView.itemsSource = _renderTextures;
     }
-}
 
-public class MyViewController : ListViewController
-{
-    private List<RenderTexture> _renderTextures;
-    private VisualTreeAsset _listItemTemplate;
-
-    public MyViewController(List<RenderTexture> renderTextures, VisualTreeAsset template) : base()
+    private void OnPointerMoveCallback(PointerMoveEvent evt)
     {
-        _renderTextures = renderTextures;
-        _listItemTemplate = template;
+        if (!Input.GetMouseButton(0))
+            return;
+
+        _lastScrollDelta = -evt.deltaPosition.y;
+        _scroller.value -= evt.deltaPosition.y;
     }
 
-    protected override VisualElement MakeItem()
+    private void OnPointerUpCallback(PointerUpEvent evt)
     {
-        return _listItemTemplate.Instantiate();
+        if (Mathf.Abs(_lastScrollDelta) < 0.1f)
+            return;
+
+        int elIndex = (int)_viewController.LastPointerDownElement.userData;
+
+        var dir = _lastScrollDelta > 0 ? 1 : -1;
+        var targetDir = (elIndex + dir) * _listView.fixedItemHeight;
+
+        StopCoroutine("ScrollTo");
+        StartCoroutine("ScrollTo", targetDir);
     }
 
-    protected override void BindItem(VisualElement element, int index)
+    IEnumerator ScrollTo(float positionY)
     {
-        RenderTexture tex = _renderTextures[index];
+        while (Mathf.Abs(_scroller.value - positionY) > 0.1f)
+        {
+            _scroller.value = Mathf.MoveTowards(_scroller.value, positionY, 0.5f / Time.deltaTime);
+            yield return null;
+        }
 
-        VisualElement visualElement = element.Q<VisualElement>("texture");
-        visualElement.style.backgroundImage = Background.FromRenderTexture(tex);
-
-        // Debug.Log($"Element '{index}' was bound");
+        _scroller.value = positionY;
     }
 }
---
+++ Assets/MyViewController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MyViewController : ListViewController
+{
+    [CanBeNull]
+    public VisualElement LastPointerDownElement { get; private set; }
+
+    private List<RenderTexture> _renderTextures;
+    private VisualTreeAsset _listItemTemplate;
+
+    public MyViewController(List<RenderTexture> renderTextures, VisualTreeAsset template) : base()
+    {
+        _renderTextures = renderTextures;
+        _listItemTemplate = template;
+    }
+
+    protected override VisualElement MakeItem()
+    {
+        return _listItemTemplate.Instantiate();
+    }
+
+    protected override void BindItem(VisualElement element, int index)
+    {
+        RenderTexture tex = _renderTextures[index];
+
+        VisualElement visualElement = element.Q<VisualElement>("texture");
+        visualElement.style.backgroundImage = Background.FromRenderTexture(tex);
+        visualElement.userData = index;
+
+        visualElement.RegisterCallback<PointerDownEvent>(PointerDownCallback);
+    }
+
+    protected override void UnbindItem(VisualElement element, int index)
+    {
+        element.UnregisterCallback<PointerDownEvent>(PointerDownCallback);
+    }
+
+    private void PointerDownCallback(PointerDownEvent evt)
+    {
+        LastPointerDownElement = evt.target as VisualElement;
+    }
+}
