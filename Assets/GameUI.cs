eff5564be3386353d38141236fcc16d8fe6234bc
Jefferson <jeffersonferreira.01@pm.me>
2023-06-27 14:55:19 -0300

    🚧 (WIP) Finally I'm able to check which list element are on screen and how much

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -11,6 +11,7 @@ public class GameUI : MonoBehaviour
     private ListView _listView;
 
     private int _i;
+    private UIDocument _doc;
 
     private void Update()
     {
@@ -19,31 +20,85 @@ public class GameUI : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
             _listView.ScrollToItem(--_i);
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            var container = _listView.Q<VisualElement>("unity-content-container");
+            var scroller = _listView.Q<Scroller>(classes: Scroller.ussClassName);
+
+            // VisualElement visualElement = _listView.contentContainer[0];
+            // Debug.Log(visualElement);
+
+            foreach (var element in container.Children())
+            {
+                var pos = element.layout.position;
+                var hei = _listView.fixedItemHeight;
+
+                float screenVisibilityFactor;
+                if (scroller.value >= pos.y)
+                {
+                    var diff = scroller.value - pos.y;
+                    screenVisibilityFactor = (hei - diff) / hei;
+                }
+                else
+                {
+                    var diff = (scroller.value + hei) - pos.y;
+                    screenVisibilityFactor = diff / hei;
+                }
+
+                // if (f > 0.5f)
+                // {
+                //     _listView.ScrollTo(element);
+                //     return;
+                // }
+            }
+        }
     }
 
     private void OnEnable()
     {
-        var doc = GetComponent<UIDocument>();
-
-        _listView = doc.rootVisualElement.Q<ListView>("main-list");
-
+        _doc = GetComponent<UIDocument>();
         // Dynamically set "list element items" height to match the whole screen
-        doc.rootVisualElement.Q<VisualElement>("unity-content-viewport")
+        _doc
+            .rootVisualElement.Q<VisualElement>("unity-content-viewport")
             .RegisterCallback<GeometryChangedEvent>(evt =>
             {
                 var visualElement = (VisualElement)evt.target;
                 _listView.fixedItemHeight = visualElement.resolvedStyle.height;
             });
 
+        _listView = _doc.rootVisualElement.Q<ListView>("main-list");
+
+        _listView.SetViewController(new MyViewController(_renderTextures, _listItemTemplate));
+
+        // Source must be set after controller
         _listView.itemsSource = _renderTextures;
-        _listView.makeItem = () => _listItemTemplate.Instantiate();
+    }
+}
 
-        _listView.bindItem = (element, index) =>
-        {
-            RenderTexture tex = _renderTextures[index];
+public class MyViewController : ListViewController
+{
+    private List<RenderTexture> _renderTextures;
+    private VisualTreeAsset _listItemTemplate;
 
-            VisualElement visualElement = element.Q<VisualElement>("texture");
-            visualElement.style.backgroundImage = Background.FromRenderTexture(tex);
-        };
+    public MyViewController(List<RenderTexture> renderTextures, VisualTreeAsset template) : base()
+    {
+        _renderTextures = renderTextures;
+        _listItemTemplate = template;
+    }
+
+    protected override VisualElement MakeItem()
+    {
+        return _listItemTemplate.Instantiate();
+    }
+
+    protected override void BindItem(VisualElement element, int index)
+    {
+        RenderTexture tex = _renderTextures[index];
+
+        VisualElement visualElement = element.Q<VisualElement>("texture");
+        visualElement.style.backgroundImage = Background.FromRenderTexture(tex);
+
+        // Debug.Log($"Element '{index}' was bound");
     }
 }
